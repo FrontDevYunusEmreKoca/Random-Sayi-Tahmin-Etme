9af9394951913a3271dae21849c2e5bab23ee4c6
Yunus Emre KOCA <yunsemrekoca@gmail.com>
2024-04-19 12:10:25 +0300

    tum duzenlemeler yapildi ve basarili bir sekilde proje tamamlandi

--- script.js
+++ script.js
@@ -1,29 +1,107 @@
 var tahminet = document.querySelector(".tahmin_et");
+document.querySelector(".tekrar-oyna ").style.display = "none";
 
-var randomNumber = Math.floor(Math.random() * 100) + 1;
-
-
+heart = 4;
+var randomSayi = Math.floor(Math.random() * 101);
 
 tahminet.onclick = function sayiTahminEt (){
-   
-    kalan_hak = 5;
     var input_sayi = document.getElementById("input_number").value;
-    console.log(input_sayi);  
+    
+   
+   
 
-  
-  
-    // for (var i=1; i <= kalan_hak.length; i++){
-        if (input_sayi < randomNumber){
-            document.getElementById("randomSayiSonuc").innerText = "Girdiğiniz Sayı Random Üretilen Sayıdan Küçüktür."
-        }
-        else if (input_sayi > randomNumber) {
-            document.getElementById("randomSayiSonuc").innerText = "Girdiğiniz Sayı Random Üretilen Sayıdan büyüktür."
-        }
-        else {
-            document.getElementById("randomSayiSonuc").innerText = "Random Sayıyı Bildiniz."
-        }
+// if (heart == 5){
+//     if (input_sayi == ""){
+//         document.getElementById("randomSayiSonuc").innerText = "Lutfen bir sayi tahmin ediniz."
+//     }
+ 
+//     else {
+//         var randomSayi = Math.floor(Math.random() * 101);
+        
+//     }
+//     if (input_sayi == randomSayi){
+//         document.getElementById("randomSayiSonuc").innerText = " Random Sayiyi Dogru bildiniz"
+//     }
+    
+//     else 
+//     {
+//         if (input_sayi > randomSayi ){
+//             document.getElementById("randomSayiSonuc").innerText = " Girdiginiz Sayi Random Sayidan buyuktur"
+//             heart--;
+//         }
+//         else {
+//             document.getElementById("randomSayiSonuc").innerText = " Girdiginiz Sayi Random Sayidan kucuktur"
+//             heart--
+//         }
+//         if (heart == 0){
+//             document.getElementById("randomSayiSonuc").innerText = " Tahmin hakkkiniz kalmamistir"
+//         }
+//     }
+// }
+if (heart != 0 ){
+   
+    if (input_sayi == ""){
+        document.getElementById("randomSayiSonuc").innerText = "  Lutfen bir tam sayi giriniz";
     }
-    console.log(randomNumber) 
+    else {
+        if (input_sayi > 100 && input_sayi < 0){
+            document.getElementById("randomSayiSonuc").innerText = "  Random Sayı 0 ile 100 arasındadır Lütfen geçerli bir sayı giriniz ";
+        }
+        else{
+            console.log("random sayi " + randomSayi);
+            if (randomSayi == input_sayi){
+                document.getElementById("randomSayiSonuc").innerText = "Random sayıyı bildiniz."
+            }
+            else {
+                if (randomSayi > input_sayi){
+                    document.getElementById("randomSayiSonuc").innerText = "Random sayı girdiğiniz sayıdan büyüktür.";
+                    var element_heart = document.querySelectorAll(".hak > i ")[heart];
+                    element_heart.classList.remove("text-danger");
+                    element_heart.classList.add("text-light");
+
+                    heart--;
+                    document.getElementById("kalan_hak").innerHTML = `<span> Kalan hakkınız <b>${heart +1}<b/>`
+                    return;
+                }
+                if (input_sayi >randomSayi){
+                    document.getElementById("randomSayiSonuc").innerText = "Random sayı girdiğiniz sayıdan küçüktür." 
+                    var element_heart = document.querySelectorAll(".hak > i ")[heart ];
+                    element_heart.classList.remove("text-danger");
+                    element_heart.classList.add("text-light");
+
+                 
+                    heart--;
+                    document.getElementById("kalan_hak").innerHTML = `<span> Kalan hakkınız <b>${heart +1}<b/>`
+                    return;
+                }
+                }
+        }
+
+
+     
+       
+    }
+   
+   
+}
+else {
+    document.getElementById("randomSayiSonuc").style.display = "none"; 
+    document.getElementById("kalan_hak").innerHTML = `<span> Tahmin Etme Hakkınız Bitmiştir. </span>`
+    document.getElementById("kalan_hak").style.paddingBottom = "20px"
+    document.querySelector(".hak").style.display = "none"
+    document.querySelector(".tekrar-oyna ").style.display = "block";
+    document.querySelector(".tahmin_et ").style.display = "none";
+}
+var tekrar_oyna = document.querySelector(".tekrar-oyna ");
+tekrar_oyna.addEventListener("click", function (){
+    location.reload();
+})
+
+
+}
+   
+    
+
        
     // }
 
